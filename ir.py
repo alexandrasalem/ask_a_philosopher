a4b3eec97980dcdc7c05976a89409a4893ac6c60
Alexandra Salem <salem@ohsu.edu>
2025-01-20 17:23:23 -0500

    Quick query set-up

--- ir.py
+++ ir.py
@@ -1,5 +1,7 @@
 import json
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.preprocessing import normalize
+import numpy as np
 
 with open('sample.json', 'r') as file:
     data = json.load(file)
@@ -8,4 +10,14 @@ corpus = [item['chapter_text'] for item in data]
 vectorizer = TfidfVectorizer()
 X = vectorizer.fit_transform(corpus)
 vectorizer.get_feature_names_out()
-print(data)
+X = normalize(X, axis=1, norm="l1")
+
+query = ["Why is the womb warm?"]
+query_vector = vectorizer.transform(query)
+query_vector = normalize(query_vector, axis=1, norm="l1")
+
+# Now the multiplication
+res = np.matmul(X.toarray(), np.transpose(query_vector.toarray()))
+
+res_data = data[np.argmax(res)]
+print(res_data)
