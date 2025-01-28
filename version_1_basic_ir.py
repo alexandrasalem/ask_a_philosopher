d8687bfdd86e09ee6428789e4ef50b0c6b00500b
Alexandra Salem <salem@ohsu.edu>
2025-01-28 15:30:40 -0500

    adding basic versions of ir task for within command line query

---
+++ version_1_basic_ir.py
@@ -0,0 +1,15 @@
+import argparse
+from ir import ir_single_query_top_doc
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("question", type=str, help="type in a question you want to ask, surrounded by quotes")
+    return parser.parse_args()
+
+def main(question):
+    res = ir_single_query_top_doc(question)
+    return res
+
+if __name__ == "__main__":
+    args = get_args()
+    main(**vars(args))
