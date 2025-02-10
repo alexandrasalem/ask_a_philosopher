7e049c716788bb89e7e2f5faf07b9c1248c1cbea
Alex Salem <alexandrasalem8@gmail.com>
2025-02-10 13:21:51 -0500

    adding functionality for evaluating multiple queries

--- ir.py
+++ ir.py
@@ -2,6 +2,7 @@ import json
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.preprocessing import normalize
 import numpy as np
+import pandas as pd
 
 def load_tfidf(corpus_json):
     """
@@ -70,5 +71,52 @@ def ir_single_query_top_doc(question,  corpus_json='aristotle.json'):
                        f'Book name: {res_data["book_label"]}\n\n'
                        f'Chapter name: {res_data["chapter_label"]}\n\n'
                        f'Chapter text: {res_data["chapter_text"]}')
+    return res_data_string
+
+def ir_multiple_query_top_doc(question_csv,  corpus_json='aristotle.json'):
+    """
+    This function pulls the doc with the highest cosine similarity to the query and returns it.
+
+    Takes as input:
+    :param question_csv: a csv with three columns: Question, Answer, Source_URL
+    :param corpus_json: location of json file with docs
+    :return: strings with the text of the docs with the highest cosine similarity to the queries. Printed as a series of text snippets.
+    """
+    data, vectorizer, X = load_tfidf(corpus_json)
+    questions = pd.read_csv(question_csv, sep=",")
+
+    queries = list(questions['Question'])
+    answers = list(questions['Answer'])
+    ids = list(questions['Answer_id'])
+    query_vector = vectorizer.transform(queries)
+    query_vector = normalize(query_vector, axis=1, norm="l1")
+
+    # Now the multiplication
+    res = np.matmul(X.toarray(), np.transpose(query_vector.toarray()))
+    res_data = np.argmax(res, axis=0)
+    res_data = res_data.astype(int)
+    res_data = [data[i] for i in res_data]
+
+    res_ids = [item["id"] for item in res_data]
+    tot = len(res_ids)
+    tot_corr = 0
+    for j in range(len(res_ids)):
+        if res_ids[j] == ids[j]:
+            tot_corr += 1
+
+    acc = tot_corr / tot
+    res_data_string = f"You got {tot_corr} out of {tot} correct, giving {acc} accuracy.\n\n"
+    res_data_string += "Below are answers to your queries:\n\n"
+    for i in range(len(res_data)):
+        res_data_string += (f'You provided the following query: {queries[i]}\n\n'
+                            f'Here is the ground truth: {answers[i]}\n\n'
+                            f'\n\n'
+                            f'Here is the closest chapter to that query:\n\n'
+                            f'Text name: {res_data[i]["text_name"]}\n\n'
+                            f'Book name: {res_data[i]["book_label"]}\n\n'
+                            f'Chapter name: {res_data[i]["chapter_label"]}\n\n'
+                            f'Chapter text: {res_data[i]["chapter_text"]}\n\n'
+                            f'-----------------------------------------\n\n')
+    #print(res_data_string)
     return res_data_string
 
