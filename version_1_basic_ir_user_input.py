711f6bb76314e0e2a8a792416838d46cade58a92
Alex Salem <alexandrasalem8@gmail.com>
2025-02-10 15:20:51 -0500

    streamlit version basic ir with user input

---
+++ version_1_basic_ir_user_input.py
@@ -0,0 +1,11 @@
+from ir import ir_single_query_top_doc
+import streamlit as st
+
+
+def main():
+    question = st.text_input("Type your question for Aristotle below. An example is provided:", "How can I lead a virtuous life?")
+    res = ir_single_query_top_doc(question)
+    st.write(res)
+
+if __name__ == "__main__":
+    main()
