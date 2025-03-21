5b16373335f5fb64805e3128c401bef9fd77ba33
Alex Salem <alexandrasalem8@gmail.com>
2025-03-21 14:12:04 -0400

    using cuda

---
+++ llm.py
@@ -0,0 +1,22 @@
+import transformers
+import torch
+
+LLM_MODEL_ID = "meta-llama/Llama-3.2-3B-Instruct"
+
+def single_query_response(question):
+    pipe = transformers.pipeline(
+        "text-generation",
+        model=LLM_MODEL_ID,
+        torch_dtype=torch.bfloat16,
+        device="cuda:1",
+    )
+    messages = [
+        {"role": "system", "content": "You are the ancient philosopher, Aristotle. Respond to this question as Aristotle would."},
+        {"role": "user", "content": question},
+    ]
+    outputs = pipe(
+        messages,
+        max_new_tokens=256,
+    )
+    text_output = outputs[0]["generated_text"][-1]
+    return text_output
