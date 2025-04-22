43078dfd6a9823c1cd51b27b713c74d2a47f79c3
Alex Salem <alexandrasalem8@gmail.com>
2025-04-21 23:17:45 -0400

    adding rag version of code

--- llm.py
+++ llm.py
@@ -1,6 +1,7 @@
 import transformers
 import torch
 import pandas as pd
+from ir import ir_single_query_top_doc
 
 LLM_MODEL_ID = "meta-llama/Llama-3.2-3B-Instruct"
 
@@ -22,15 +23,21 @@ def single_query_response(question, prompt = "You are the ancient philosopher, A
     text_output = outputs[0]["generated_text"][-1]
     return text_output
 
-def multiple_query_responses(question_csv, prompt = "You are the ancient philosopher, Aristotle. Respond to this question as Aristotle would."):
+def multiple_query_responses(question_csv, ir = True, prompt = "You are the ancient philosopher, Aristotle. Respond to this question as Aristotle would. Answer with 3-5 sentences at most."):
     questions = pd.read_csv(question_csv, sep=",")
     queries = list(questions['Questions'])
     queries = [query.lower() for query in queries]
 
     responses = []
+    prompts = []
     for query in queries:
+        if ir:
+            doc = ir_single_query_top_doc(query)
+            prompt = f"{prompt} Consider this relevant chapter from Aristotle's work when crafting your response: {doc}"
         text_output = single_query_response(query, prompt)['content']
         responses.append(text_output)
+        prompts.append(prompt)
 
     questions['llm_response'] = responses
+    questions['llm_prompt'] = prompts
     return questions
---
+++ version_4_rag_multiple_questions.py
@@ -0,0 +1,27 @@
+import argparse
+import os
+from llm import multiple_query_responses
+import streamlit as st
+from datetime import datetime
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("question_csv", type=str, help="Location of csv file with questions")
+    parser.add_argument("--use_streamlit", action="store_true", help="Whether to use streamlit")
+    return parser.parse_args()
+
+def main(question_csv, use_streamlit=False):
+    res = multiple_query_responses(question_csv)
+    current_date = datetime.now()
+    date_string = current_date.strftime("%Y-%m-%d-%H-%M-%S")
+    os.makedirs(date_string, exist_ok=False)
+    filename = f'{date_string}/{question_csv[:-4]}_responses.csv'
+    res.to_csv(filename, index=False)
+    if use_streamlit:
+        st.write(res)
+    print(res)
+    return res
+
+if __name__ == "__main__":
+    args = get_args()
+    main(**vars(args))
