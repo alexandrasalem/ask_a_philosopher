2db5b800c6ee392b97645cbf29ca6c9bd9c8280c
Alex Salem <alexandrasalem8@gmail.com>
2025-10-15 16:51:12 -0400

    testing using runpod

---
+++ rp_handler_ask_a_phil.py
@@ -0,0 +1,52 @@
+from llm import single_query_response
+from ir import ir_single_query_top_doc
+import runpod
+import time
+
+prompt = "You are the ancient philosopher, Aristotle. Respond to this question as Aristotle would."
+
+def process_input_llm(question, prompt):
+    #llm_res = single_query_response(question, prompt = prompt)
+    res = f"Your question: {question}\n"
+    res += "Answer:\n"
+    #res += llm_res['content']
+    return res
+
+def process_input_rag(question, prompt):
+    doc = ir_single_query_top_doc(question)
+    prompt = f"{prompt} Consider this relevant chapter from Aristotle's work when crafting your response: {doc}"
+    llm_res = single_query_response(question, prompt = prompt)
+    res = f"Your question: {question}\n"
+    res += "Answer:\n"
+    res += llm_res['content']
+    return res
+
+
+def handler(event):
+    #   This function processes incoming requests to your Serverless endpoint.
+    #
+    #    Args:
+    #        event (dict): Contains the input data and request metadata
+    #
+    #    Returns:
+    #       Any: The result to be returned to the client
+
+    # Extract input data
+    print(f"Worker Start")
+    input = event['input']
+
+    question = input.get('question')
+    #seconds = input.get('seconds', 0)
+
+    print(f"Received question: {question}")
+    #print(f"Sleeping for {seconds} seconds...")
+
+    # You can replace this sleep call with your own Python code
+    res = process_input_llm(question = question, prompt = prompt)
+
+    return res
+
+
+# Start the Serverless function when the script is run
+if __name__ == '__main__':
+    runpod.serverless.start({'handler': handler})
